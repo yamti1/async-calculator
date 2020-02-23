9843a04d6e332a9843e220cf171ae068b50f0f84
yam t <yam.tirosh@gmail.com>
2020-02-23 15:54:34 +0200

    Write sub handler

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ app = Sanic("My App")
 
 
 @app.route("/add")
-async def root(request):
+async def add(request):
     return text(sum(request.json))
+
+
+@app.route("/sub")
+async def sub(request):
+    a, b = request.json
+    return text(a - b)
 
 
 if __name__ == '__main__':
